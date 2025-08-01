b1ea64f3a8ea11ec062ca190d66381b6caf1a842
Darya Hryneuskaya <darya.hryneuskaya@gmail.com>
2025-08-01 16:40:18 +0200

    commit 1

---
+++ src/MyMain.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//one-line comment
+/*
+multiple-line comments
+ */
+public class MyMain {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.print("Hi!"); // use Hi text
+        System.out.println("\n");
+    }
+}
